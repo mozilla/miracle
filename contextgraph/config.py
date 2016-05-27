6a2cb2073b83db4ac7c8af4b791c178d2812a9df
Hanno Schlichting <hanno@hannosch.eu>
2016-05-27 19:27:26 +0200

    More CI setup fixes.

--- contextgraph/config.py
+++ contextgraph/config.py
@@ -1,6 +1,6 @@
 import os
 
 TESTING = 'TESTING' in os.environ
-REDIS_HOST = os.environ.get('REDIS_HOST')
+REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
 REDIS_DB = '1' if TESTING else '0'
 REDIS_URI = 'redis://%s:6379/%s' % (REDIS_HOST, REDIS_DB)
